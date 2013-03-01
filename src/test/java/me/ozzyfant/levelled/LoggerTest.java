681b1e7ff54a485481571cc8428501ebd8a4d0fd
Crosant <crosant@gmx.net>
2013-03-01 13:43:17 +0100

    Added tests for logging

---
+++ src/test/java/me/ozzyfant/levelled/LoggerTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ozzyfant.levelled;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Florian Reichmuth
+ */
+public class LoggerTest extends TestCase {
+    
+    public LoggerTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of info method, of class Logger.
+     */
+    public void testInfo() {
+        System.out.println("info");
+        String message = "Test Info Logging";
+        Logger instance = new Logger();
+        instance.info(message);
+    }
+
+    /**
+     * Test of severe method, of class Logger.
+     */
+    public void testSevere() {
+        System.out.println("severe");
+        String message = "Test Server Logging";
+        Logger instance = new Logger();
+        instance.severe(message);
+    }
+}
